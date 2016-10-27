28fb314b227f2378da09b522d86d38c254a17d99
Soorya Narayan <nayaran.ayroos@gmail.com>
2016-10-27 15:36:37 +0530

    Add state views
    Add state view classes to restrict access
    Rename Point to Vector2D
    Rename point project to physics project
    Change leader in path_planner_helper from pointer to id
    Add tower class

--- src/state/include/player_state_handler/player_state_handler.h
+++ src/state/include/player_state_handler/player_state_handler.h
@@ -3,27 +3,52 @@
 
 #include <memory>
 #include <cstdint>
-#include "point.h"
+#include "vector2d.h"
 #include "terrain/terrain_element.h"
 #include "state_export.h"
 
 namespace state {
 
 struct TerrainElementView {
-	point::Point position;
+	physics::Vector2D position;
 	int64_t size;
 	TERRAIN_TYPE terrain_type;
 	LOS_TYPE los_type;
 };
 
-class STATE_EXPORT PlayerStateHandler {
+class UnitView {
+private:
+
+public:
+	physics::Vector2D GetPosition();
+	int64_t GetId();
+	int64_t GetHp();
+	int64_t GetSpeed();
+	int64_t GetAttackTargetId();
+	physics::Vector2D GetVelocity();
+	physics::Vector2D GetPosition();
+};
+
+
+class STATE_EXPORT StateView {
 private:
 	std::shared_ptr<State> state;
+public:
+	std::vector<int64_t> GetUnitIds();
+	std::vector<int64_t> GetEnemyIds();
+	std::vector<int64_t> GetTowerIds();
+	std::vector<int64_t> GetFlagIds();
+	TerrainElementView XYToTerrainElement();
+	TerrainElementView OffsetToTerrainElement();
+	Actor
+};
+
+
+class STATE_EXPORT PlayerStateHandler {
+private:
 	int64_t player_id;
 public:
 	PlayerStateHandler();
-	TerrainElementView XYToTerrainElement();
-	TerrainElementView OffsetToTerrainElement();
 	void MoveUnits();
 	void AttackUnit();
 	int64_t PlanPath();
