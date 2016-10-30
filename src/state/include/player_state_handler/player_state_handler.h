a014c15f9565d2b245a3c8a6bbbc7bc9babd97db
JHurricane96 <arun.jhurricane@gmail.com>
2016-10-30 14:53:25 +0530

    Add docs for player_state_handler.h

--- src/state/include/player_state_handler/player_state_handler.h
+++ src/state/include/player_state_handler/player_state_handler.h
@@ -1,3 +1,11 @@
+/**
+ * @file player_state_handler.h
+ * Definitions for the state handler for players
+ * 
+ * The state handler object via which players access the internal game
+ * state.
+ */
+
 #ifndef STATE_PLAYER_STATE_HANDLER_PLAYER_STATE_HANDLER_H
 #define STATE_PLAYER_STATE_HANDLER_PLAYER_STATE_HANDLER_H
 
@@ -12,6 +20,11 @@
 
 namespace state {
 
+/**
+ * Player view for TerrainElement
+ *
+ * @see TerrainElement
+ */
 struct TerrainElementView {
 	physics::Vector2D position;
 	int64_t size;
@@ -19,15 +32,33 @@ struct TerrainElementView {
 	LOS_TYPE los_type;
 };
 
+/**
+ * Player view for PathPlannerHelper
+ *
+ * @see PathPlannerHelper
+ */
 class PathPlannerHelperView {
 private:
+	/**
+	 * Pointer to underlying PathPlannerHelper instance which the view
+	 * provides an interface for.
+	 */
 	std::shared_ptr<PathPlannerHelper> path_planner_helper;
 public:
 	bool IsPathPlanning();
 };
 
+/**
+ * Player view for Actors that are movable.
+ *
+ * @see Actor
+ */
 class UnitView {
 private:
+	/**
+	 * Pointer to underlying Actor instance which the view provides an
+	 * interface for.
+	 */
 	std::shared_ptr<Actor> unit;
 public:
 	physics::Vector2D GetPosition();
@@ -41,8 +72,17 @@ public:
 	void SetAttackTargetId(act_id_t attack_target_id);
 };
 
+/**
+ * Player view for the State.
+ *
+ * @see State
+ */
 class STATE_EXPORT StateView {
 private:
+	/**
+	 * Pointer to underlying State instance which the view provides an
+	 * interface for.
+	 */
 	std::shared_ptr<State> state;
 public:
 	StateView(State &state);
@@ -55,18 +95,40 @@ public:
 	UnitView GetUnitFromId();
 };
 
+/**
+ * Handler for the player to access and modify the internal game state.
+ * 
+ * Restricts player access to the internal game state. Also maintains
+ * a separate copy of the state to which changes are made. The changed
+ * state must be merged back periodically. Therefore, this class is 
+ * both a view and a buffer.
+ */
 class STATE_EXPORT PlayerStateHandler {
 private:
+	/**
+	 * A restricted view of the state with appropriate getters.
+	 */
 	const StateView state_view;
+	/**
+	 * The player ID to whom the handler belongs.
+	 */
 	act_id_t player_id;
 public:
+	/**
+	 * The constructor.
+	 *
+	 * @param[in] state The state object to create a view for.
+	 */
 	PlayerStateHandler(State &state);
 	void MoveUnits(
 		list_act_id_t unit_ids,
 		physics::Vector2D destination,
 		const std::unique_ptr<Formation> formation_type
 	);
-	void AttackUnit(list_act_id_t attacker_ids, act_id_t attack_target_id);
+	void AttackUnit(
+		list_act_id_t attacker_ids,
+		act_id_t attack_target_id
+	);
 	int64_t PlanPath();
 	StateView GetState();
 };
