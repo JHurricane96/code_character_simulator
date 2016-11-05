e2bf76be33faa9ca3600f857c63fe20b64491721
Sachin <ediblepeople@gmail.com>
2016-11-05 15:07:11 +0530

    Add IPC headers
    Define basic structure of DataTransfer class

---
+++ src/ipc/include/ipc.h
@@ -0,0 +1,42 @@
+/**
+ * @file ipc.h
+ * Function declarations for inter process communication methods between renderer and simulator
+*/
+#ifndef IPC_IPC_H
+#define IPC_IPC_H
+
+#include<memory>
+#include<atomic>
+#include<string>
+#include "state.h"
+
+namespace ipc {
+
+/**
+ * Inter process communication methods between renderer and simulator
+ * Utilises Protocol Buffers for serializing structured data
+ */
+
+class DataTransfer {
+
+public:
+       /**
+        * Sends a boolean to the simulator to trigger a start simulation/stop simulation event
+        * @param[in]  InterruptSignal    true for start, false for pause
+        */
+        void InterruptCall (bool InterruptSignal);
+        /**
+         * Sends an std::State variable to the renderer
+         * @param[in]  StateVar    Internal state of the game
+         */
+        void StateTransfer (std::shared_ptr<state::State> StateVar);
+        /**
+         * Sends an std::string to the renderer which contains debug output present on simulation process's stdout
+         * @param[in]  DebugOut    Process's stdout stream data piped to the renderer
+         */
+        void Debug(std::string DebugOut);
+
+    };
+
+}
+#endif // IPC_IPC_H
