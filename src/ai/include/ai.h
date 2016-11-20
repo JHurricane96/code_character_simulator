3205f01da05c3b647cbc4901a1e39f1857e4094b
Sachin <sachinashok.g@gmail.com>
2016-11-20 12:14:56 +0530

    Define AI class

---
+++ src/ai/include/ai.h
@@ -0,0 +1,29 @@
+#ifndef AI_AI_H
+#define AI_AI_H
+
+#include <memory>
+#include <map>
+#include "player_ai_helper.h"
+#include "ai_export.h"
+
+namespace ai {
+
+class Group;
+
+class AI_EXPORT AI : public player::PlayerAiHelper {
+private:
+	bool init_groups;
+	state::list_act_id_t all_actors;
+	std::vector<std::pair<int64_t, int>> sortedEnemies;
+	std::vector<state::act_id_t> kingsGuard, flagsGuard;
+	std::vector <Group*> groups;
+public:
+	AI();
+	void Update(std::shared_ptr<state::PlayerStateHandler> state) override;
+	void SetSortedEnemies(std::shared_ptr<state::PlayerStateHandler> state);
+};
+
+
+
+}
+#endif //AI_AI_H
