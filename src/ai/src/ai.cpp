63fbbdb77ab30e106b8b0634e1c780227b0d7a69
cheeni666 <vshreenivasbharadwaj@gmail.com>
2016-11-22 13:34:45 +0530

    Guard state update and select state functions declaration

--- src/ai/src/ai.cpp
+++ src/ai/src/ai.cpp
@@ -112,4 +112,23 @@ GroupState* Attack::SelectState(state::act_id_t unitId, std::shared_ptr<state::P
 	return NULL; 
 }
 
+GroupState* Guard::update(
+	state::act_id_t unitId, 
+	std::shared_ptr<state::PlayerStateHandler> state_handler, 
+	std::vector<std::pair<int64_t, int>> sortedEnemies,
+	std::vector<state::act_id_t> &kingsGuard,
+	std::vector<state::act_id_t> &flagsGuard
+) {
+	groupUtilityHolder = 0;
+	guard_rules -> Strategy(unitId, state_handler, kingsGuard, flagsGuard, groupUtilityHolder, sortedEnemies);
+	return SelectState(unitId, state_handler);
+}
+
+GroupState* Guard::SelectState(state::act_id_t unitId, std::shared_ptr<state::PlayerStateHandler> state_handler) {
+	int new_state_no = guard_rules -> Utility(groupUtilityHolder);
+	if(new_state_no == 1)
+		//return new Attack;
+	return NULL;
+}
+
 }
