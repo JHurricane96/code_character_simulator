33b8d85e562257fcc7a9815a658ddb9c9893bd14
Soorya Narayan <nayaran.ayroos@gmail.com>
2016-11-30 19:26:55 +0530

    Add methods for grid neighbours

--- src/state/include/terrain/terrain.h
+++ src/state/include/terrain/terrain.h
@@ -24,6 +24,15 @@ private:
 	 * A 2D matrix of TerrainElements
 	 */
 	std::vector<std::vector<TerrainElement> > grid;
+	/**
+	 * A helper vector that holds offsets to adjacent grid neighbours
+	 */
+	std::vector<physics::Vector2D> adj_neighbours;
+	/**
+	 * A helper vector that holds offsets to diagonal grid neighbours
+	 */
+	std::vector<physics::Vector2D> dia_neighbours;
+
 public:
 	Terrain(int64_t nrows);
 	Terrain(std::vector<std::vector<TerrainElement> > grid);
@@ -50,6 +59,33 @@ public:
 	 * @return     A 2d vector to the bottom right of the grid
 	 */
 	physics::Vector2D GetSize();
+	/**
+	 * Gets the adjacent neighbours of a given TerrainElement
+	 *
+	 * @param[in]  offset  The grid offsets of the TerrainElement
+	 *                     whose neighbours are needed
+	 *
+	 * @return     The adjacent neighbours
+	 */
+	std::vector<physics::Vector2D> GetAdjacentNeighbours(physics::Vector2D offset);
+	/**
+	 * Gets the diagonal neighbours of a given TerrainElement
+	 *
+	 * @param[in]  offset  The grid offset of the TerrainElement
+	 *                     whose neighbours are needed
+	 *
+	 * @return     The diagonal neighbours
+	 */
+	std::vector<physics::Vector2D> GetDiagonalNeighbours(physics::Vector2D offset);
+	/**
+	 * Gets all the neighbours of a given TerrainElement
+	 *
+	 * @param[in]  offset  The grid offset of the TerrainElement
+	 *                     whose neighbours are needed
+	 *
+	 * @return     The neighbours
+	 */
+	std::vector<physics::Vector2D> GetAllNeighbours(physics::Vector2D offset);
 
 };
 
