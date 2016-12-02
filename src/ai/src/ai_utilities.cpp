b6cc6cf1e5e0447870907c900300c3e36d3f40e9
Sachin <sachinashok.g@gmail.com>
2016-12-02 21:43:11 +0530

    Make GetEnemyAllyHpRatio smarter

--- src/ai/src/ai_utilities.cpp
+++ src/ai/src/ai_utilities.cpp
@@ -37,4 +37,108 @@ float GetEnemyAllyHpRatioByThreshold(
 	return enemySum/allySum;
 }
 
+float GetEnemyAllyHpRatio (
+	std::shared_ptr<state::PlayerStateHandler> state,
+	state::act_id_t id,
+	int k
+)	{
+		state::UnitView parentUnit = state->GetUnitFromId(id, nullptr);
+
+		std::vector<state::MagicianView> allyMagicians = state->GetMagicians();
+		std::vector<state::SwordsmanView> allySwordsmen = state->GetSwordsmen();
+		std::vector<state::TowerView> allyTowers = state->GetTowers();
+
+		std::vector<state::EnemyMagicianView> enemyMagicians = state->GetEnemyMagicians();
+		std::vector<state::EnemySwordsmanView> enemySwordsmen = state->GetEnemySwordsmen();
+		std::vector<state::EnemyTowerView> enemyTowers = state->GetEnemyTowers();
+
+		std::vector<std::pair<int64_t, float>> allyPairs;
+		std::vector<std::pair<int64_t, float>> enemyPairs;
+
+		for (auto mage : allyMagicians) {
+			allyPairs.push_back (
+				std::make_pair (
+					mage.GetHp(),
+					mage.GetPosition().distance(parentUnit.GetPosition())
+				)
+			);
+		}
+		for (auto swordsman : allySwordsmen) {
+			allyPairs.push_back (
+				std::make_pair (
+					swordsman.GetHp(),
+					swordsman.GetPosition().distance(parentUnit.GetPosition())
+				)
+			);
+		}
+		for (auto tower : allyTowers) {
+			allyPairs.push_back (
+				std::make_pair (
+					tower.GetHp(),
+					tower.GetPosition().distance(parentUnit.GetPosition())
+				)
+			);
+		}
+		for (auto enemyMage : enemyMagicians) {
+			enemyPairs.push_back (
+				std::make_pair (
+					enemyMage.GetHp(),
+					enemyMage.GetPosition().distance(parentUnit.GetPosition())
+				)
+			);
+		}
+		for (auto enemySwordsman : enemySwordsmen) {
+			enemyPairs.push_back (
+				std::make_pair (
+					enemySwordsman.GetHp(),
+					enemySwordsman.GetPosition().distance(parentUnit.GetPosition())
+				)
+			);
+		}
+		for (auto enemyTower : enemyTowers) {
+			enemyPairs.push_back (
+				std::make_pair (
+					enemyTower.GetHp(),
+					enemyTower.GetPosition().distance(parentUnit.GetPosition())
+				)
+			);
+		}
+
+		std::sort(allyPairs.begin(), allyPairs.end(), SortedBySecondElement);
+		std::sort(enemyPairs.begin(), enemyPairs.end(), SortedBySecondElement);
+
+		float allyHp = 0, enemyHp = 0;
+		int allyCount = 0, enemyCount = 0;
+
+		if(allyPairs.size() + enemyPairs.size() < k)
+			k = allyPairs.size() + enemyPairs.size();
+
+		for (int i = 0; i < k; i++) {
+			if (allyCount < allyPairs.size() && enemyCount < enemyPairs.size()) {
+				if (enemyPairs[i].second < allyPairs[i].second) {
+					enemyHp += enemyPairs[i].first;
+					enemyCount++;
+				}
+				else {
+					allyHp += allyPairs[i].first;
+					allyCount++;
+				}
+			}
+			else if (allyCount < allyPairs.size()) {
+				allyHp += allyPairs[i].first;
+				allyCount++;
+			}
+			else if (enemyCount < enemyPairs.size()) {
+				enemyHp += enemyPairs[i].first;
+				enemyCount++;
+			}
+		}
+		if(enemyCount == 0)
+			return -1;
+		if (allyHp == 0)
+			allyHp++;
+
+		return (enemyHp / allyHp);
+}
+
 }
