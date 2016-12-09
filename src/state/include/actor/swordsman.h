4de96667c0ea67693b5d1c12602494e28bddf698
JHurricane96 <arun.jhurricane@gmail.com>
2016-12-09 22:34:30 +0530

    Shift PathPlannerHelper to Actor class
    TODO: Create separate Unit class for movable Actors

--- src/state/include/actor/swordsman.h
+++ src/state/include/actor/swordsman.h
@@ -9,7 +9,6 @@
 #include <cstdint>
 #include <memory>
 #include "actor/actor.h"
-#include "path_planner/path_planner_helper.h"
 #include "state_export.h"
 
 namespace state {
@@ -17,16 +16,6 @@ namespace state {
 class STATE_EXPORT Swordsman: public Actor {
 public:
 	Swordsman();
-	/**
-	 * An instance of the PathPlannerHelper class to help with path planning
-	 */
-	PathPlannerHelper path_planner_helper;
-	/**
-	 * Gets the path planner helper
-	 *
-	 * @return     The path planner helper
-	 */
-	const std::unique_ptr<PathPlannerHelper> GetPathPlannerHelper();
 	/**
 	 * Update function to be called every tick
 	 *
