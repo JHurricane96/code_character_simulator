bf9804a4e71ea69c1ce7f330f233b5faef70fbde
Sachin <sachinashok.g@gmail.com>
2016-12-09 17:11:34 +0530

    Populate terrain grid message
    Set values for each terrain element
    Fix lack of terrain field in proto file
    Pass function callback to retrieve terrain object for populating terrain

--- src/ipc/src/populate.cc
+++ src/ipc/src/populate.cc
@@ -2,44 +2,104 @@
 #include <string>
 #include "state.pb.h"
 
-int PopulateTerrain(IPC::State* StateMessage, std::shared_ptr<state::State>* StateVar) {
+/**
+ * Populates the terrain
+ *
+ * Terrain is a grid where each unit (terrain element) consists of position
+ * size, terrain type, line of sight for a given player and timestamp denoting
+ * last visit by the players
+ *
+ * @param[in]  StateVar        the state object
+ * @param[in]  TerrainMessage  the terrain message object
+ *
+ * @return     Exit status
+ */
+int PopulateTerrain(IPC::Terrain* TerrainMessage, std::shared_ptr<state::State>* StateVar) {
 
 	/**
-	 * Set position of terrain element
-	 *
-	 * @param[in]  position  The position vector
+	 * Iterator to traverse the terrain grid by row
 	 */
-	StateMessage->set_allocated_position(StateVar->GetPosition());
+	std::vector< std::vector<TerrainElement> >::iterator row;
 	/**
-	 * Set size of terrain element
-	 *
-	 * @param[in]  size  The side length
+	 * Iterator to traverse the terrain grid's row by column
 	 */
-	StateMessage->set_size(StateVar->GetSize());
+	std::vector<TerrainElement>::iterator col;
+
 	/**
-	 * Set size of terrain element
-	 *
-	 * @param[in]  terrain_type  The terrain typ
+	 * 2D vector to store the terrain grid
 	 */
-	StateMessage->set_terrain_type(StateVar->GetTerrainType());
+	std::vector<std::vector<TerrainElement> > terrain_grid = StateVar->terrain;
+
 	/**
-	 * Set the line of sight for a given player
-	 *
-	 *  @param[in]  player_id  The id of the player
+	 * Add a grid message
 	 */
-	StateMessage->set_los_type_player_1(StateVar->GetLos(player_id));
-	StateMessage->set_los_type_player_2(StateVar->GetLos(player_id));
+	IPC::TerrainRows* terrain_grid_message = terrain_message->add_grid();
+
 	/**
-	 * Set the the timestamps noting when the element was last visited by 
-	 * the players
-	 *
-	 *  @param[in]  player_id  The id of the player
+	 * Loop through the grid to fill the terrain element messages unit by unit
 	 */
-	StateMessage->set_last_seen_player_1(StateVar->GetLastSeen(player_id));
-	StateMessage->set_last_seen_player_2(StateVar->GetLastSeen(player_id));
+
+	for (row = terrain_grid.begin(); row!= terrain_grid.end(); ++row) {
+		for (col = row.begin(); col!= row.end(); ++col) {
+
+			/**
+			 * Vector2D to store and map coordinate to terrain element
+			 */
+			physics::Vector2D position;
+			/**
+			 * To find x and y coordinates of the grid
+			 */
+			double x_value = std::distance(terrain_grid.begin(), row);
+			double y_value = std::distance(row.begin(), col);
+
+			position->x = x_value;
+			position->y = y_value;
+
+			/**
+			 * Add a terrain element message in grid message
+			 */
+			IPC::TerrainElement* terrain_element_message = terrain_grid_message->add_terrain_element();
+
+			state::TerrainElement terrain_element_object = terrain_grid->CoordinateToTerrainElement(position);
+
+			/**
+			 * Set position of terrain element
+			 */
+			terrain_element_message->set_allocated_position(terrain_element_object->GetPosition());
+			/**
+			 * Set size of terrain element
+			 */
+			terrain_element_message->set_size(terrain_element_object->GetSize());
+			/**
+			 * Set size of terrain element
+			 */
+			terrain_element_message->set_terrain_type(terrain_element_object->GetTerrainType());
+			/**
+			 * Set the line of sight for a given player
+			 */
+			terrain_element_message->set_los_type_player_1(terrain_element_object->GetLos(player_id));
+			terrain_element_message->set_los_type_player_2(terrain_element_object->GetLos(player_id));
+			/**
+			 * Set the the timestamps noting when the element was last visited by
+			 * the players
+			 */
+			terrain_element_message->set_last_seen_player_1(terrain_element_object->GetLastSeen(player_id));
+			terrain_element_message->set_last_seen_player_2(terrain_element_object->GetLastSeen(player_id));
+		}
+	}
 
 	return 0;
 }
+
+/**
+ * Populates the state message
+ *
+ * State message consists of actors & terrain
+ *
+ * @param[in]  StateVar  the state object
+ *
+ * @return     Exit status
+ */
 
 int PopulateState(std::shared_ptr<state::State> StateVar) {
 
@@ -51,7 +111,7 @@ int PopulateState(std::shared_ptr<state::State> StateVar) {
 
 	IPC::State StateMessage;
 
-	if (PopulateTerrain(&state, &StateVar) < 0) {
+	if (PopulateTerrain(StateMessage->set_allocated_terrain(), &StateVar) < 0) {
 		std::cerr << "Failed to load terrain" <<endl;
 		return -1;
 	}
