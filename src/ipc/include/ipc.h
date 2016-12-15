18f7809cb21f6bd535d76af6844fac4adcd0ff53
Sachin <sachinashok.g@gmail.com>
2016-12-15 08:10:32 +0530

    Extend DataTransfer class
    Add function declaration for UpdateLevel in ipc header
    Restrict DataTransfer class functions to ipc namespace
    Verify protobuf version in all API functions
    Rename numerous functions

--- src/ipc/include/ipc.h
+++ src/ipc/include/ipc.h
@@ -1,6 +1,7 @@
 /**
  * @file ipc.h
- * Function declarations for inter process communication methods between renderer and simulator
+ * Function declarations for inter process communication methods between
+ * renderer and simulator
 */
 #ifndef IPC_IPC_H
 #define IPC_IPC_H
@@ -19,23 +20,41 @@ namespace ipc {
 class DataTransfer {
 
 public:
-       /**
-        * Sends a boolean to the simulator to trigger a start simulation/stop simulation event
-        * @param[in]  InterruptSignal    true for start, false for pause
-        */
-        void InterruptCall (bool InterruptSignal);
-        /**
-         * Sends an std::State variable to the renderer
-         * @param[in]  StateVar    Internal state of the game
-         */
-        void StateTransfer (std::shared_ptr<state::State> StateVar);
-        /**
-         * Sends an std::string to the renderer which contains debug output present on simulation process's stdout
-         * @param[in]  DebugOut    Process's stdout stream data piped to the renderer
-         */
-        void Debug(std::string DebugOut);
-
-    };
+		/**
+		* Sends a boolean to the simulator to trigger a start simulation/stop
+		* simulation event
+		*
+		* @param[in]  PlayPauseStatus  true for start, false for pause
+		*
+		* @return     Exit status
+		*/
+		int InterruptCall (bool PlayPauseStatus);
+		/**
+		* Sends level number to the simulator
+		*
+		* @param[in]  LevelNumber  current level number
+		*
+		* @return     Exit status
+		*/
+		int UpdateLevel (int LevelNumber);
+		/**
+		 * Sends an std::string to the renderer which contains debug output
+		 * present on simulation process's stdout
+		 *
+		 * @param[in]  DebugOut    Process's stdout piped to the renderer
+		 *
+		 * @return     Exit status
+		 */
+		int DebugLogs (std::string DebugOut);
+		/**
+		 * Sends an std::State variable to the renderer
+		 *
+		 * @param[in]  StateVar    Internal state of the game
+		 *
+		 * @return     Exit status
+		 */
+		int StateTransfer (std::shared_ptr<state::State> StateVar);
+	};
 
 }
 #endif // IPC_IPC_H
