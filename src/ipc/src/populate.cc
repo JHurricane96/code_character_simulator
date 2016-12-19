7dcebdd70a6f18632b83ba5528047e6ca7000b1c
Sachin <sachinashok.g@gmail.com>
2016-12-19 12:43:21 +0530

    Redefine proto definition

--- src/ipc/src/populate.cc
+++ src/ipc/src/populate.cc
@@ -5,261 +5,26 @@
 #include <iostream>
 #include <string>
 #include "state.h"
-#include "utilities.h"
 #include "ipc.h"
 #include "state.pb.h"
 
 /**
- * Populates the terrain
+ * Populates the actors
  *
- * Terrain is a grid where each unit (terrain element) consists of position
- * size, terrain type, line of sight for a given player and timestamp denoting
- * last visit by the players
+ * State actors are stored as repeated Actor messages as part of the State message
  *
  * @param[in]  StateVar        the state object
- * @param[in]  TerrainMessage  the terrain message object
+ * @param[in]  ActorMessage    the actor message object
  *
  * @return     Exit status
  */
-int PopulateTerrain(IPC::Terrain* TerrainMessage, std::shared_ptr<state::State>* StateVar) {
+int PopulateActors(IPC::Actor* ActorMessage, std::shared_ptr<state::State>* StateVar) {
 
-	/**
-	 * Iterator to traverse the terrain grid by row
-	 */
-	std::vector< std::vector<TerrainElement> >::iterator row;
-	/**
-	 * Iterator to traverse the terrain grid's row by column
-	 */
-	std::vector<TerrainElement>::iterator col;
-
-	/**
-	 * 2D vector to store the terrain grid
-	 */
-	std::vector<std::vector<TerrainElement> > terrain_grid = StateVar->terrain;
-
-	/**
-	 * Add a grid message
-	 */
-	IPC::TerrainRows* terrain_grid_message = TerrainMessage->add_grid();
-
-	/**
-	 * Loop through the grid to fill the terrain element messages unit by unit
-	 */
-
-	for (row = terrain_grid.begin(); row!= terrain_grid.end(); ++row) {
-		for (col = row.begin(); col!= row.end(); ++col) {
-
-			/**
-			 * Vector2D to store and map coordinate to terrain element
-			 */
-			physics::Vector2D position, translated_position;
-			/**
-			 * To find x and y coordinates of the grid
-			 */
-			double x_value = std::distance(terrain_grid.begin(), row);
-			double y_value = std::distance(row.begin(), col);
-
-			position->x = x_value;
-			position->y = y_value;
-
-			/**
-			 * Add a terrain element message in grid message
-			 */
-			IPC::TerrainElement* terrain_element_message = terrain_grid_message->add_terrain_element();
-
-			state::TerrainElement terrain_element_object = terrain_grid->CoordinateToTerrainElement(position);
-
-			translated_position = terrain_element_object->GetPosition();
-
-			/**
-			 * Set position of terrain element
-			 */
-			IPC::Vector2D element_position = terrain_element_message->set_allocated_position();
-			element_position.set_x(translated_position.x);
-			element_position.set_y(translated_position.y);
-			/**
-			 * Set size of terrain element
-			 */
-			terrain_element_message->set_size(terrain_element_object->GetSize());
-			/**
-			 * Set size of terrain element
-			 */
-			terrain_element_message->set_terrain_type(terrain_element_object->GetTerrainType());
-			/**
-			 * Set the line of sight for a given player
-			 */
-			terrain_element_message->set_los_type_player_1(terrain_element_object->GetLos(player_id));
-			terrain_element_message->set_los_type_player_2(terrain_element_object->GetLos(player_id));
-			/**
-			 * Set the the timestamps noting when the element was last visited by
-			 * the players
-			 */
-			terrain_element_message->set_last_seen_player_1(terrain_element_object->GetLastSeen(player_id));
-			terrain_element_message->set_last_seen_player_2(terrain_element_object->GetLastSeen(player_id));
-		}
-	}
 
 	return 0;
 }
 
-/**
- * Populates the tower log message
- *
- * Towerlog contains tower_id, timestamp and owner player_id
- *
- * @param[in]  StateVar              the state object
- * @param[in]  TowerCaptureLogEntry  the tower capture log message object
- *
- * @return     Exit status
- */
-int PopulateTowerLog(std::shared_ptr<state::State>* StateVar, IPC::TowerCaptureLogEntry* TowerCaptureLogMessage) {
-
-	std::vector<TowerCaptureLogEntry> TowerCaptureLogEntry = StateVar->GetTowerCaptureLog();
-
-	/**
-	 * Iterator to traverse the log entry tuple
-	 */
-	std::vector<TowerCaptureLogEntry>::iterator it;
-
-	for(it = TowerCaptureLogEntry.begin(); it!= TowerCaptureLogEntry.end(); ++it) {
-
-		TowerCaptureLogMessage->set_tower_id(TowerCaptureLogEntry[it].tower_id);
-		TowerCaptureLogMessage->set_timestamp(TowerCaptureLogEntry[it].timestamp);
-		TowerCaptureLogMessage->set_player_id(TowerCaptureLogEntry[it].player_id);
-	}
-}
-
-/**
- * Populates the state message with details like player 1(2) unit_ids,
- * player 1(2) visible enemy unit ids, tower ids and flag ids
- *
- * @param[in]  StateVar      the state object
- * @param[in]  StateMessage  the state message object
- *
- * @return     Exit status
- */
-int PopulateTowerLog(std::shared_ptr<state::State>* StateVar, IPC::State StateMessage) {
-
-	list_act_id_t Player1Ids      = StateVar->GetPlayer1Ids();
-	list_act_id_t Player2Ids      = StateVar->GetPlayer2Ids();
-	list_act_id_t Player1EnemyIds = StateVar->GetPlayer1EnemyIds();
-	list_act_id_t Player2EnemyIds = StateVar->GetPlayer2EnemyIds();
-	list_act_id_t TowerIds        = StateVar->GetTowerIds();
-	list_act_id_t FlagIds         = StateVar->GetFlagIds();
-
-	/**
-	 * Iterator to traverse the vectors
-	 */
-	list_act_id_t::iterator it;
-
-	for(it = Player1Ids.begin(); it!= Player1Ids.end(); ++it) {
-
-		StateMessage->set_player_1_unit_ids(Player1Ids[it]);
-	}
-	for(it = Player2Ids.begin(); it!= Player2Ids.end(); ++it) {
-
-		StateMessage->set_player_2_unit_ids(Player2Ids[it]);
-	}
-	for(it = Player1EnemyIds.begin(); it!= Player1EnemyIds.end(); ++it) {
-
-		StateMessage->set_player_1_visible_enemy_unit_ids(Player1EnemyIds[it]);
-	}
-	for(it = Player2EnemyIds.begin(); it!= Player2EnemyIds.end(); ++it) {
-
-		StateMessage->set_player_2_visible_enemy_unit_ids(Player2EnemyIds[it]);
-	}
-	for(it = TowerIds.begin(); it!= TowerIds.end(); ++it) {
-
-		StateMessage->set_tower_ids(TowerIds[it]);
-	}
-	for(it = FlagIds.begin(); it!= FlagIds.end(); ++it) {
-
-		StateMessage->set_flag_ids(FlagIds[it]);
-	}
-}
-
 namespace ipc {
-
-	/**
-	 * Populates the utility message with the play/pause status
-	 *
-	 * Utility messages can be user logs, play/pause status or level number
-	 * @param[in]  play_pause_status  current status of renderer/simulator
-	 *
-	 * @return     Exit status
-	 */
-	int DataTransfer::InterruptCall(bool PlayPauseStatus)
-	{
-		/**
-		 * Verify that the version of the library that we linked against is
-		 * compatible with the version of the headers we compiled against
-		 */
-		GOOGLE_PROTOBUF_VERIFY_VERSION;
-
-		IPC::Utilities Utility;
-
-		Utility.set_play_pause_status(PlayPauseStatus);
-
-		if (!Utility.SerializeToOstream(std::cout)) {
-			cerr << "Failed to transfer play/pause status" << endl;
-			return -1;
-		}
-		return 0;
-	}
-
-	/**
-	 * Populates the utility message with the level number
-	 *
-	 * Utility messages can be user logs, play/pause status or level number
-	 * @param[in]  play_pause_status  current status of renderer/simulator
-	 *
-	 * @return     Exit status
-	 */
-	int DataTransfer::UpdateLevel(int LevelNumber)
-	{
-		/**
-		 * Verify that the version of the library that we linked against is
-		 * compatible with the version of the headers we compiled against
-		 */
-		GOOGLE_PROTOBUF_VERIFY_VERSION;
-
-		IPC::Utilities Utility;
-
-		Utility.set_level_number(LevelNumber);
-
-		if (!Utility.SerializeToOstream(std::cout)) {
-			cerr << "Failed to transfer level number" << endl;
-			return -1;
-		}
-		return 0;
-	}
-
-	/**
-	 * Populates the utility message with the user debug output
-	 *
-	 * Utility messages can be user logs, play/pause status or level number
-	 * @param[in]  play_pause_status  current status of renderer/simulator
-	 *
-	 * @return     Exit status
-	 */
-	int DataTransfer::DebugLogs(std::string DebugOut)
-	{
-		/**
-		 * Verify that the version of the library that we linked against is
-		 * compatible with the version of the headers we compiled against
-		 */
-		GOOGLE_PROTOBUF_VERIFY_VERSION;
-
-		IPC::Utilities Utility;
-
-		Utility.set_user_logs(DebugOut);
-
-		if (!Utility.SerializeToOstream(std::cout)) {
-			cerr << "Failed to transfer user logs" << endl;
-			return -1;
-		}
-		return 0;
-	}
 
 	/**
 	 * Populates the state message
@@ -281,23 +46,13 @@ namespace ipc {
 
 		IPC::State StateMessage;
 
-		if (PopulateTerrain(&StateVar, StateMessage->set_allocated_terrain()) < 0) {
+		if (PopulateActors(&StateVar, StateMessage->set_allocated_terrain()) < 0) {
 			std::cerr << "Failed to load terrain" << endl;
 			return -1;
 		}
 
-		if (PopulateTowerLog(&StateVar, StateMessage->add_tower_capture_log()) < 0) {
-			std::cerr << "Failed to load tower log" << endl;
-			return -1;
-		}
-
-		if (PopulateStateDetails(&StateVar, &StateMessage) < 0) {
-			std::cerr << "Failed to load state details" << endl;
-			return -1;
-		}
-
 		if (!StateMessage.SerializeToOstream(std::cout)) {
-			cerr << "Failed to transfer state message" << endl;
+			std::cerr << "Failed to transfer state message" << endl;
 			return -1;
 		}
 		return 0;
