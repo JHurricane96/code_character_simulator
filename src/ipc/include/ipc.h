919024f62b3e912f9b510a8d53a3d8935496a7bc
Sachin <sachinashok.g@gmail.com>
2016-12-21 15:12:21 +0530

    Get actor lists from state

--- src/ipc/include/ipc.h
+++ src/ipc/include/ipc.h
@@ -6,8 +6,6 @@
 #ifndef IPC_IPC_H
 #define IPC_IPC_H
 
-#include <memory>
-#include <string>
 #include "state.h"
 
 namespace ipc {
@@ -20,32 +18,6 @@ namespace ipc {
 class DataTransfer {
 
 public:
-		/**
-		* Sends a boolean to the simulator to trigger a start simulation/stop
-		* simulation event
-		*
-		* @param[in]  PlayPauseStatus  true for start, false for pause
-		*
-		* @return     Exit status
-		*/
-		int InterruptCall (bool PlayPauseStatus);
-		/**
-		* Sends level number to the simulator
-		*
-		* @param[in]  LevelNumber  current level number
-		*
-		* @return     Exit status
-		*/
-		int UpdateLevel (int LevelNumber);
-		/**
-		 * Sends an std::string to the renderer which contains debug output
-		 * present on simulation process's stdout
-		 *
-		 * @param[in]  DebugOut    Process's stdout piped to the renderer
-		 *
-		 * @return     Exit status
-		 */
-		int DebugLogs (std::string DebugOut);
 		/**
 		 * Sends an std::State variable to the renderer
 		 *
