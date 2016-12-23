ac476407a0025a3aab35038fa24be875e8cee52f
JHurricane96 <arun.jhurricane@gmail.com>
2016-12-23 18:03:34 +0530

    Remove PathPlannerHelper from King definition
    PathPlannerHelper has been shifted to Actor

--- src/state/include/actor/king.h
+++ src/state/include/actor/king.h
@@ -6,9 +6,7 @@
 #ifndef STATE_ACTOR_KING_H
 #define STATE_ACTOR_KING_H
 
-#include <memory>
 #include "actor/actor.h"
-#include "path_planner/path_planner_helper.h"
 #include "state_export.h"
 
 namespace state {
@@ -20,16 +18,6 @@ namespace state {
 class STATE_EXPORT King: public Actor {
 public:
 	King();
-	/**
-	 * An instance of the PathPlannerHelper class to help with path planning
-	 */
-	PathPlannerHelper path_planner_helper;
-	/**
-	 * Gets the path planner helper
-	 *
-	 * @return     The path planner helper
-	 */
-	const std::unique_ptr<PathPlannerHelper> GetPathPlannerHelper();
 	/**
 	 * Update function to be called every tick
 	 */
