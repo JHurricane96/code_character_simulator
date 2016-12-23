96b9e081165b31aa68a03940b8ccb51caa9e6b49
Soorya Narayan <nayaran.ayroos@gmail.com>
2016-12-23 14:13:58 +0530

    Add constructor for Swordsman

--- src/state/src/actor/swordsman.cpp
+++ src/state/src/actor/swordsman.cpp
@@ -3,7 +3,36 @@
 
 namespace state {
 
-Swordsman::Swordsman() : Actor() {}
+Swordsman::Swordsman(
+		act_id_t id,
+		PlayerId player_id,
+		int64_t attack,
+		int64_t hp,
+		int64_t max_hp,
+		int64_t max_speed,
+		int64_t size,
+		int64_t total_respawn_time,
+		int64_t time_to_respawn,
+		int64_t time_spent_near_base,
+		physics::Vector2D position,
+		physics::Vector2D velocity,
+		int64_t los_radius
+	):
+	Actor(
+		id,
+		player_id,
+		attack,
+		hp,
+		max_hp,
+		max_speed,
+		size,
+		total_respawn_time,
+		time_to_respawn,
+		time_spent_near_base,
+		position,
+		velocity,
+		los_radius
+	) {}
 
 void Swordsman::Update(int64_t delta_time) {
 	position = position + velocity * delta_time;
