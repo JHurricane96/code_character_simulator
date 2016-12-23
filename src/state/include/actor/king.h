cdddf37095438ee9b668cd074205b1f1056afd8d
JHurricane96 <arun.jhurricane@gmail.com>
2016-12-24 00:03:07 +0530

    Add constructor definitions for some Actors
    Added for:
    * Base
    * King
    * Flag

--- src/state/include/actor/king.h
+++ src/state/include/actor/king.h
@@ -18,7 +18,21 @@ namespace state {
  */
 class STATE_EXPORT King: public Actor {
 public:
-	King();
+	King(
+		act_id_t id,
+		PlayerId player_id,
+		int64_t attack,
+		int64_t hp,
+		int64_t max_hp,
+		int64_t max_speed,
+		int64_t size,
+		int64_t total_respawn_time,
+		int64_t time_to_respawn,
+		int64_t time_spent_near_base,
+		physics::Vector2D position,
+		physics::Vector2D velocity,
+		int64_t los_radius
+	);
 	/**
 	 * Update function to be called every tick
 	 *
