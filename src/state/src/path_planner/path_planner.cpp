ffc4af1977035273c159b1a421b8115226cfd86e
JHurricane96 <arun.jhurricane@gmail.com>
2016-12-23 15:31:02 +0530

    Add definitions for PathPlanner class

---
+++ src/state/src/path_planner/path_planner.cpp
@@ -0,0 +1,72 @@
+#include "path_planner/path_planner.h"
+
+namespace state {
+
+PathPlanner::PathPlanner(int64_t map_size) : graph(map_size) {
+	formations = std::vector<std::vector<Formation> >(2);
+}
+
+void PathPlanner::MakeFormation(
+	PlayerId player_id,
+	std::vector<std::shared_ptr<Actor> > &units,
+	Terrain &terrain,
+	std::shared_ptr<FormationMaker> formation_maker,
+	physics::Vector2D destination,
+	std::vector<int64_t> terrain_weights
+) {
+	std::vector<physics::Vector2D> destinations;
+	PlanPath(
+		units[0]->GetPosition(),
+		destination,
+		terrain,
+		destinations,
+		terrain_weights
+	);
+	formations[player_id].push_back(Formation(
+		player_id,
+		next_formation_id[player_id]++,
+		units,
+		formation_maker,
+		destinations
+	));
+}
+
+int64_t PathPlanner::PlanPath(
+	physics::Vector2D start_point,
+	physics::Vector2D destination,
+	Terrain &terrain,
+	std::vector<physics::Vector2D> &next_points,
+	std::vector<int64_t> terrain_weights
+) {
+	return graph.FindPath(
+		start_point,
+		destination,
+		terrain,
+		next_points,
+		terrain_weights
+	);
+}
+
+void PathPlanner::Update(
+	std::vector<std::vector<std::shared_ptr<Actor> > > &sorted_units
+) {
+	for (
+		int64_t cur_player_id = 0;
+		cur_player_id < formations.size();
+		++cur_player_id
+	) {
+		std::vector<Formation> new_formations;
+		for (auto formation : formations[cur_player_id]) {
+			formation.Update(
+				sorted_units[cur_player_id],
+				sorted_units[(cur_player_id + 1) % LAST_PLAYER]
+			);
+			if (!formation.IsFinished()) {
+				new_formations.push_back(formation);
+			}
+		}
+		formations[cur_player_id] = new_formations;
+	}
+}
+
+}
