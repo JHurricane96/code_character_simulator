28f0cb27360f90104dce306c09c8fd462b95c6c7
JHurricane96 <arun.jhurricane@gmail.com>
2016-12-23 15:32:08 +0530

    Add definitions for State class

---
+++ src/state/src/state.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include "state.h"
+
+namespace state {
+
+State::State() : terrain(1), path_planner(1) {}
+
+void State::MoveUnits(
+	list_act_id_t unit_ids,
+	physics::Vector2D destination,
+	std::shared_ptr<FormationMaker> formation_maker,
+	std::vector<int64_t> terrain_weights
+) {
+	std::vector<std::shared_ptr<Actor> > units;
+	for (auto unit_id : unit_ids) {
+		units.push_back(actors[unit_id]);
+	}
+
+	path_planner.MakeFormation(
+		units[0]->GetPlayerId(),
+		units,
+		terrain,
+		formation_maker,
+		destination,
+		terrain_weights
+	);
+}
+
+bool CompareActorsByXCoordinate(
+	std::shared_ptr<Actor> a,
+	std::shared_ptr<Actor> b
+) {
+	return (a->GetPosition().x < b->GetPosition().x);
+}
+
+void State::Update(int64_t delta_time) {
+	for (auto units : sorted_actors) {
+		std::sort(
+			units.begin(),
+			units.end(),
+			CompareActorsByXCoordinate
+		);
+	}
+
+	path_planner.Update(sorted_actors);
+
+	for (auto actor : actors) {
+		actor->Update(delta_time);
+	}
+}
+
+}
