e40cdb25e1f0d048a2befbca80e432bc69dd045f
JHurricane96 <arun.jhurricane@gmail.com>
2016-12-24 11:08:40 +0530

    Add definitions for Flag class

---
+++ src/state/src/actor/flag.cpp
@@ -0,0 +1,55 @@
+#include "actor/flag.h"
+#include "actor/king.h"
+
+namespace state {
+
+Flag::Flag(
+		act_id_t id,
+		PlayerId player_id,
+		int64_t attack,
+		int64_t hp,
+		int64_t max_hp,
+		int64_t max_speed,
+		int64_t size,
+		int64_t total_respawn_time,
+		int64_t time_to_respawn,
+		int64_t time_spent_near_base,
+		physics::Vector2D position,
+		physics::Vector2D velocity,
+		int64_t los_radius
+	):
+	Actor(
+		id,
+		player_id,
+		attack,
+		hp,
+		max_hp,
+		max_speed,
+		size,
+		total_respawn_time,
+		time_to_respawn,
+		time_spent_near_base,
+		position,
+		velocity,
+		los_radius
+	), king(nullptr) {}
+
+void Flag::Capture(King * king) {
+	this->king = king;
+}
+
+void Flag::Drop() {
+	king = nullptr;
+}
+
+bool Flag::IsCaptured() {
+	return (king != nullptr);
+}
+
+void Flag::Update(int64_t delta_time) {
+	if (IsCaptured()) {
+		position = king->GetPosition();
+	}
+}
+
+}
