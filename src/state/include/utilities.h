fe2e76e7b8e36df9c8c25a3bb7f96d9fad72ad3f
Soorya Narayan <nayaran.ayroos@gmail.com>
2016-12-31 09:32:40 +0530

    Add helper function to flatten vectors

--- src/state/include/utilities.h
+++ src/state/include/utilities.h
@@ -11,6 +11,15 @@
 typedef int64_t act_id_t;
 typedef std::vector<int64_t> list_act_id_t;
 
+template<typename T>
+std::vector<T> flatten(const std::vector<std::vector<T>> &orig)
+{
+	std::vector<T> ret;
+	for(const auto &v: orig)
+		ret.insert(ret.end(), v.begin(), v.end());
+	return ret;
+}
+
 enum PlayerId
 {
 	PLAYER1 = 0,
