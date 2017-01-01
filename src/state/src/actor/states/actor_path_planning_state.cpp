0ae46e3655e07616050b21b447d961948e0ebe0e
JHurricane96 <arun.jhurricane@gmail.com>
2017-01-02 03:40:07 +0530

    Add definitions for ActorPathPlanningState class

---
+++ src/state/src/actor/states/actor_path_planning_state.cpp
@@ -0,0 +1,33 @@
+#include "actor/states/actor_path_planning_state.h"
+#include "actor/states/actor_pursuit_state.h"
+#include "actor/states/actor_dead_state.h"
+#include "actor/states/actor_idle_state.h"
+#include "actor/actor.h"
+#include "path_planner/path_planner_helper.h"
+
+namespace state {
+
+void ActorPathPlanningState::Enter(Actor * actor) {}
+
+std::unique_ptr<ActorState> ActorPathPlanningState::Update(
+	Actor * actor,
+	int64_t delta_time
+) {
+	if (actor->GetHp() <= 0) {
+		return std::unique_ptr<ActorState>(new ActorDeadState());
+	}
+	else if (actor->GetAttackTarget() != nullptr) {
+		return std::unique_ptr<ActorState>(new ActorPursuitState());
+	}
+	else if (!actor->GetPathPlannerHelper()->IsPathPlanning()) {
+		return std::unique_ptr<ActorState>(new ActorIdleState());
+	}
+
+	return nullptr;
+}
+
+void ActorPathPlanningState::Exit(Actor * actor) {
+	actor->GetPathPlannerHelper()->FinishPath();
+}
+
+}
