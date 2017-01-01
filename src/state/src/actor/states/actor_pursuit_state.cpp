952d662b5d2c83c0b9107f710501b5b585c70a2f
JHurricane96 <arun.jhurricane@gmail.com>
2017-01-02 03:40:30 +0530

    Add definitions for ActorPursuitState class

---
+++ src/state/src/actor/states/actor_pursuit_state.cpp
@@ -0,0 +1,49 @@
+#include "actor/states/actor_pursuit_state.h"
+#include "actor/states/actor_attack_state.h"
+#include "actor/states/actor_path_planning_state.h"
+#include "actor/states/actor_idle_state.h"
+#include "actor/states/actor_dead_state.h"
+#include "actor/actor.h"
+#include "path_planner/path_planner_helper.h"
+
+namespace state {
+
+void ActorPursuitState::Enter(Actor * actor) {}
+
+std::unique_ptr<ActorState> ActorPursuitState::Update(
+	Actor * actor,
+	int64_t delta_time
+) {
+	auto target = actor->GetAttackTarget();
+	if (actor->GetHp() <= 0) {
+		actor->SetVelocity(physics::Vector2D(0, 0));
+		return std::unique_ptr<ActorState>(new ActorDeadState());
+	}
+	else if (actor->GetPathPlannerHelper()->IsPathPlanning()) {
+		actor->StopAttack();
+		return std::unique_ptr<ActorState>(
+			new ActorPathPlanningState()
+		);
+	}
+	else if (target == nullptr || target->IsDead()) {
+		actor->SetVelocity(physics::Vector2D(0, 0));
+		actor->StopAttack();
+		return std::unique_ptr<ActorState>(new ActorIdleState());
+	}
+	else if (actor->GetPosition().distance(target->GetPosition())
+		<= actor->GetSize() + target->GetSize()
+	) {
+		actor->SetVelocity(physics::Vector2D(0, 0));
+		return std::unique_ptr<ActorState>(new ActorAttackState());
+	}
+
+	auto to_target = target->GetPosition() - actor->GetPosition();
+	actor->SetVelocity(
+		to_target * actor->GetMaxSpeed() / to_target.magnitude()
+	);
+	return nullptr;
+}
+
+void ActorPursuitState::Exit(Actor * actor) {}
+
+}
