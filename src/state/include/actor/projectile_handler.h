52032207acf917f9c629a361d3060f6ade48c14a
Soorya Narayan <nayaran.ayroos@gmail.com>
2017-01-06 05:22:00 +0530

    Add Projectile Handler class declaration

---
+++ src/state/include/actor/projectile_handler.h
@@ -0,0 +1,106 @@
+/**
+ * @file projectile_handler.h
+ *
+ * Contains the projectile handler class
+ */
+
+#ifndef STATE_ACTOR_PROJECTILE_HANDLER_H
+#define STATE_ACTOR_PROJECTILE_HANDLER_H
+
+#include "actor/actor.h"
+#include "actor/arrow.h"
+#include "actor/tower.h"
+#include "state_export.h"
+#include "terrain/terrain.h"
+#include "utilities.h"
+#include "vector2d.h"
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+namespace state {
+/**
+ * Class for handling all projectiles
+ */
+class STATE_EXPORT ProjectileHandler {
+private:
+	/**
+	 * A vector of arrows that the Projectile Handler is handling at present
+	 */
+	std::vector<std::shared_ptr<Arrow> > arrows;
+	/**
+	 * Holds the value of the next arrow's actor id
+	 */
+	int64_t next_arrow_id;
+	/**
+	 * A helper function to handle Actor updates
+	 *
+	 * Iterates through towers and creates arrows if the Tower has an
+	 * attack target set
+	 *
+	 * @param      towers   The towers
+	 * @param      terrain  The terrain
+	 */
+	void HandleActors(
+		std::vector<std::vector<std::shared_ptr<Tower> > >& towers,
+		Terrain* terrain);
+	/**
+	 * A helper function to handle Arrow updates
+	 *
+	 * Calls the arrow's update function and removes an arrow if it has
+	 * reached destination
+	 *
+	 * @param[in]  delta_time  The delta time
+	 */
+	void HandleArrows(int64_t delta_time);
+
+public:
+	/**
+	 * Constructor for the Projectile Handler
+	 *
+	 * @param[in]  first_arrow_id  The ID for the first arrow to be
+	 * 							   instantiated
+	 */
+	ProjectileHandler(int64_t first_arrow_id);
+	/**
+	 * Creates an arrow to be fired
+	 *
+	 * @param[in]  player_id      The ID of the player who fires
+	 * @param[in]  start          The start position
+	 * @param      attack_target  The actor to attack
+	 * @param[in]  max_speed      The maximum speed of the arrow
+	 * @param[in]  ttl            The life time of the arrow (in clock ticks)
+	 * @param[in]  size            The size of the arrow
+	 * @param[in]  damage         The damage the arrow can cause
+	 */
+	void CreateArrow(
+		PlayerId player_id,
+		physics::Vector2D start,
+		Actor* attack_target,
+		int64_t max_speed,
+		int64_t ttl,
+		int64_t size,
+		int64_t damage
+	);
+	/**
+	 * Handles the arrow states
+	 *
+	 * Cycles through actors and creates arrows if the Archer or Tower has a
+	 * attack target set
+	 *
+	 * Updates the arrow position if the arrow hasn't reached its destination
+	 * yet; and if it has reached, inflicts damage on the target
+	 *
+	 * @param[in]  delta_time  The delta time
+	 * @param      actors      vector of actors
+	 * @param      terrain     The terrain
+	 */
+	void Update(
+		int64_t delta_time,
+		std::vector<std::vector<std::shared_ptr<Tower> > >& towers,
+		Terrain* terrain
+	);
+};
+}
+
+#endif
