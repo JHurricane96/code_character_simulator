bd8aabba5baa7a19a00a9be3300c65a8dbc18019
Soorya Narayan <nayaran.ayroos@gmail.com>
2017-01-06 05:48:17 +0530

    Add methods to tower class declaration
    Add Respwan method to deal with new tower owner
    Add Contend method to deal with tower contention

--- src/state/include/actor/tower.h
+++ src/state/include/actor/tower.h
@@ -8,18 +8,20 @@
 
 #include <cstdint>
 #include "actor/actor.h"
+#include "actor/arrow.h"
 #include "state_export.h"
+
+namespace state {
 
 /**
  * Describes the possible owners of the tower
  */
 enum class TowerOwner{
-	UNOWNED,
 	PLAYER1,
 	PLAYER2,
+	UNOWNED,
 };
 
-namespace state {
 /**
  * Class for Towers
  */
@@ -29,8 +31,121 @@ private:
 	 * The tower's owner
 	 */
 	TowerOwner tower_owner;
+	/**
+	 * Radius around tower that units need to be present to contend for
+	 * tower ownership
+	 * Measured in coordinates
+	 */
+	int64_t contention_radius;
+	/**
+	 * The contention score a player must reach to attain tower ownership
+	 * Calculated as sum of difference in number of units over time
+	 */
+	int64_t max_contention_score;
+	/**
+	 * The present contention score
+	 * Varies between -max_contention_score and max_contention_score
+	 */
+	int64_t contention_score;
+	/**
+	 * Speed of an arrow fired from the tower
+	 */
+	int64_t arrow_speed;
+	/**
+	 * Time to live for the arrows fired
+	 */
+	int64_t arrow_ttl;
+	/**
+	 * Size of the arrows fired
+	 */
+	int64_t arrow_size;
+	/**
+	 * True if ready to fire arrow, false otherwise
+	 */
+	bool ready_to_attack;
 public:
-	Tower();
+	Tower (
+	    act_id_t id,
+	    PlayerId player_id,
+	    int64_t attack,
+	    int64_t hp,
+	    int64_t max_hp,
+	    int64_t max_speed,
+	    int64_t size,
+	    int64_t total_respawn_time,
+	    int64_t time_to_respawn,
+	    int64_t time_spent_near_base,
+	    physics::Vector2D position,
+	    physics::Vector2D velocity,
+	    int64_t los_radius,
+	    int64_t attack_speed,
+	    int64_t contention_radius,
+	    int64_t max_contention_score,
+	    int64_t range,
+	    int64_t arrow_speed,
+	    int64_t arrow_ttl,
+	    int64_t arrow_size
+	);
+	/**
+	 * The Tower's death method
+	 *
+	 * Similar to Actor's death method except
+	 * - position isn't reset
+	 * - contention_time_elapsed is set to 0
+	 */
+	void Die() override;
+	/**
+	 * Determines if ready to attack.
+	 * Getter for ready_to_attack
+	 *
+	 * @return     True if ready to attack, False otherwise.
+	 */
+	bool IsReadyToAttack();
+	/**
+	 * Gets an arrow's lifetime
+	 *
+	 * @return     The timt to live for the arrow
+	 */
+	int64_t GetArrowTtl();
+	/**
+	 * Gets the tower's owner
+	 *
+	 * @return     The tower's owner
+	 */
+	TowerOwner GetTowerOwner();
+	/**
+	 * Gets the speed of an arrow fired from the tower
+	 *
+	 * @return     The arrow speed
+	 */
+	int64_t GetArrowSpeed();
+	/**
+	 * Gets the size of an arrow fired from the tower
+	 *
+	 * @return     The arrow size
+	 */
+	int64_t GetArrowSize();
+	/**
+	 * Respawns Tower
+	 *
+	 * @param[in]  pid   The tower owner id
+	 */
+	void Respawn(TowerOwner pid);
+	/**
+	 * Is called when units are fighting over a tower after its health has
+	 * been depleted to zero
+	 *
+	 * When one player's units completely occupy the vicinity of the tower,
+	 * that player attains ownership of the tower
+	 *
+	 * @param[in]  actors  The actors owned by the players
+	 *
+	 * @return     True if new owner is settled, else false
+	 */
+	bool Contend(
+		int64_t delta_time,
+		std::vector<std::vector<std::shared_ptr<Actor> > > actors
+	);
 	/**
 	 * Update function to be called every tick
 	 *
