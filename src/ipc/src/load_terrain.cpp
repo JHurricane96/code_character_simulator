91fc54af6487d26cc60c60b7746a31cd3d8336d6
Sachin <sachinashok.g@gmail.com>
2017-01-08 21:32:12 +0530

    Clean up code

--- src/ipc/src/load_terrain.cpp
+++ src/ipc/src/load_terrain.cpp
@@ -14,41 +14,51 @@
 
 using namespace std;
 
-const int te_size = 20;
+const int TerrainSize = 20;
 
-state::Terrain DepopulateTerrain(const IPC::Terrain& RetrievedMessage) {
+/**
+ * Depopulates the terrain
+ * 
+ * Terrain is a grid where each unit (terrain element) consists of position
+ * size, terrain type, line of sight for a given player and timestamp denoting
+ * last visit by the players
+ *
+ * @param[in]  TerrainMessage  the IPC::Terrain message object
+ * @param[out] TerrainObject   the state::Terrain object 
+ */
+state::Terrain DepopulateTerrain(const IPC::Terrain& TerrainMessage) {
 
 	vector<vector<state::TerrainElement> > Grid;
 
-	//cout<<"This is the terrain speaking\n";
-	for (int i = 0; i < RetrievedMessage.row_size(); i++)
+	/**
+	 * Iterates through the TerrainMessage object and fills in the Grid with
+	 * position, size and type of terrain
+	 */
+	for (int i = 0; i < TerrainMessage.row_size(); i++)
 	{
-		const IPC::Terrain::TerrainRow& RowMessage = RetrievedMessage.row(i);
+		const IPC::Terrain::TerrainRow& RowMessage = TerrainMessage.row(i);
 		vector<state::TerrainElement> Row;
 		for(int j = 0; j < RowMessage.element_size(); j++)
 		{
-			//std::this_thread::sleep_for(std::chrono::seconds(1));
+
 			const IPC::Terrain::TerrainElement& ElementMessage = RowMessage.element(j);
+
 			IPC::Terrain::Vector2D Position(ElementMessage.position());
-			//cout<<"Position: ("<<Position.x()<<","<<Position.y()<<") ";
-			//cout<<"Size: "<<ElementMessage.size()<<" ";
+			
 			state::TERRAIN_TYPE terrain_type;
 			switch(ElementMessage.type())
 			{
 				case IPC::Terrain::TerrainElement::PLAIN : 
-					//cout<<"type: PLAIN\n";
 					terrain_type = state::PLAIN;
 					break;
 				case IPC::Terrain::TerrainElement::FOREST : 
-					//cout<<"type: FOREST\n";
 					terrain_type = state::FOREST;
 					break;
 				case IPC::Terrain::TerrainElement::MOUNTAIN : 
-					//cout<<"type: MOUNTAIN\n";
 					terrain_type = state::MOUNTAIN;
 					break;
 			}
-			Row.push_back(state::TerrainElement(terrain_type, physics::Vector2D(i * te_size, j * te_size), te_size));
+			Row.push_back(state::TerrainElement(terrain_type, physics::Vector2D(i * TerrainSize, j * TerrainSize), TerrainSize));
 		}
 		Grid.push_back(Row);
 	}
@@ -75,15 +85,15 @@ namespace ipc {
 		 */
 		GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-		IPC::Terrain RetrievedMessage;
+		IPC::Terrain TerrainMessage;
 
 		fstream input("terrain_level1.txt", ios::in | ios::binary);
 
-		if (!RetrievedMessage.ParseFromIstream(&input)) {
+		if (!TerrainMessage.ParseFromIstream(&input)) {
 			cerr << "Failed to retrieve state message" << endl;
 			return -1;
 		}
 
-		return ( DepopulateTerrain(RetrievedMessage) );
+		return ( DepopulateTerrain(TerrainMessage) );
 	}
 }
