4a25a47522d37cb68b40796242a9d51c6ae0fad2
Soorya Narayan <nayaran.ayroos@gmail.com>
2017-01-10 08:08:30 +0530

    Add Archer Class declaration

--- src/state/include/actor/archer.h
+++ src/state/include/actor/archer.h
@@ -19,31 +19,88 @@ namespace state {
 class STATE_EXPORT Archer: public Actor {
 private:
 	/**
-	 * The max max range of an arrow shot by the archer
+	 * Speed of an arrow fired by the archer
 	 */
-	int64_t range;
+	int64_t arrow_speed;
+	/**
+	 * Time to live for the arrows fired
+	 */
+	int64_t arrow_ttl;
+	/**
+	 * Size of the arrows fired
+	 */
+	int64_t arrow_size;
+	/**
+	 * True if ready to fire arrow, false otherwise
+	 */
+	bool ready_to_attack;
 public:
-	Archer();
+	Archer (
+	    act_id_t id,
+	    PlayerId player_id,
+	    int64_t attack,
+	    int64_t hp,
+	    int64_t max_hp,
+	    int64_t max_speed,
+	    int64_t size,
+	    int64_t total_respawn_time,
+	    int64_t time_to_respawn,
+	    int64_t time_spent_near_base,
+	    physics::Vector2D position,
+	    physics::Vector2D velocity,
+	    int64_t los_radius,
+	    int64_t attack_speed,
+	    int64_t range,
+	    int64_t arrow_speed,
+	    int64_t arrow_ttl,
+	    int64_t arrow_size
+	);
 	/**
-	 * Gets the max range of an arrow shot by the archer
+	 * The archer's attack function
+	 * Sets the ready_to_attack bool to true
+	 */
+	void Attack() override;
+	/**
+	 * The archer's StopAttack function
 	 *
-	 * @return     The range
+	 * Is called when target is dead
+	 * Sets ready_to_attack bool to false
+	 * Sets attack_target to nullptr
 	 */
-	int64_t GetRange();
+	void StopAttack() override;
 	/**
-	 * An instance of the PathPlannerHelper class to help with path planning
-	 */
-	PathPlannerHelper path_planner_helper;
-	/**
-	 * Gets the path planner helper
+	 * Determines if ready to attack.
+	 * Getter for ready_to_attack
 	 *
-	 * @return     The path planner helper
+	 * @return     True if ready to attack, False otherwise.
 	 */
-	const std::unique_ptr<PathPlannerHelper> GetPathPlannerHelper();
+	bool IsReadyToAttack();
+	/**
+	 * Gets an arrow's lifetime
+	 *
+	 * @return     The time to live for the arrow
+	 */
+	int64_t GetArrowTtl();
+	/**
+	 * Gets the speed of an arrow fired by the archer
+	 *
+	 * @return     The arrow speed
+	 */
+	int64_t GetArrowSpeed();
+	/**
+	 * Gets the size of an arrow fired from the archer
+	 *
+	 * @return     The arrow size
+	 */
+	int64_t GetArrowSize();
+	/**
+	 * Sets ready_to_attack to false
+	 */
+	void SetReadyToAttackToFalse();
 	/**
 	 * Update function to be called every tick
 	 */
-	void Update();
+	void Update(int64_t delta_time);
 };
 
 }
