9a17eafda1b875769a5aa694e65e5264a6accdb8
prajwalkr <prajwalrenukanand@gmail.com>
2017-01-13 01:00:46 +0530

    Added main AI class

--- src/ai/src/ai.cpp
+++ src/ai/src/ai.cpp
@@ -1,12 +1,48 @@
-class Group {
+#include <vector>
 
+typedef int64_t act_id_t;
+typedef std::vector<int64_t> list_act_id_t;
+
+/**
+  * The Main AI Class
+  */
+class AI 
+{
+public:
+	list_act_id_t all_actors;
+	vector <Group*> groups;
+	AI() 
+	{
+		// todo: initialise list of actor ids before passing
+		Group *begin = Group(all_actors);
+	}
+
+	void update() 
+	{
+		for (auto &group : groups) group -> update();
+	}
+}
+
+class Group : AI
+{
 private:
 	GroupState* state;
+	list_act_id_t group_actors;	// actors in this group
 public:
-	virtual void update(Group& unit) 
+	Group(list_act_id_t actors) 
+	{
+		this->group_actors = actors;
+	}
+
+	virtual void update() 
 	{
 		state->update(*this);
 	};
+
+	void split()
+	{
+		
+	}
 	virtual void AttackUtility() {};
 	virtual void RetreatUtility() {};
 	virtual void ExploreUtility() {};
