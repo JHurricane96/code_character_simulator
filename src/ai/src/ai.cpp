79a15435d1f06c9c2ca2efe342b4b54e8f889f58
prajwalkr <prajwalrenukanand@gmail.com>
2017-01-13 12:36:01 +0530

    Basic implementation of merge and split functions of groups complete

--- src/ai/src/ai.cpp
+++ src/ai/src/ai.cpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <functional>
 #include <set>
+#include <algorithm>
+#include <cmath>
 
 #define mod 1000000000
 typedef int64_t act_id_t;
@@ -35,11 +37,12 @@ private:
 	GroupState* state;
 	list_act_id_t group_actors;	// actors in this group
 public:
-	Group(list_act_id_t actors) 
+	Group(list_act_id_t actors, GroupState& cur_state=GroupState()) 
 	{
 		group_actors = actors;
 		group_id = rand() % mod;
-		groups[group_id] = *this;	
+		groups[group_id] = *this;
+		state = cur_state;
 	}
 
 	virtual void update() 
@@ -52,17 +55,86 @@ public:
 		}
 	};
 
+	/*** Split, Merge and their utilities below this **/
+
+	/**
+	  * There can be at most one call to either split or merge function per time tick
+	  * If multiple calls have to be made make sure these multiple calls operate on 
+	    disjoint sets of actors. Not doing so would lead to runtime errors.
+	  * The split function splits a group, given the HP distribution of the new groups.
+	  * The merge function has to be passed a GroupState* indicating the state from which
+	  	the new Group can resume their actions. Not doing so, would reset all of them to a
+	  	default state.
+	  */
+	
 	void split(vector <int> hps)
 	{
 		sort(hps.begin(), hps.end(), std::greater<int>());
 		set < pair <int, act_id_t> > group_hps;
+		vector < list_act_id_t > actor_splits(hps.size());
+
 		for (auto &id: group_actors) 
 		{
-			int hp = // get actor hp
+			int hp = // get current actor hp
 			group_hps.insert(make_pair(hp, id));
 		}
+		// clip extra HP from requested distribution
+		hps = clipExtra(hps, accumulate(group_hps.begin(), group_hps.end(), 0));
 
-		for ()
+		int cur = 0; // index of HP in hps array being allocated
+		int cur_split_index = 0;
+
+		for (int i = 0; i < hps.size(); ++i)
+		{
+			int req_hp = hps[i];
+			bool empty = true;
+			set < pair <int, act_id_t> > :: iterator allocated = getCeil(group_hps, req_hp);
+			while (alloc_hp != 0)
+			{
+				actor_splits[i].append(allocated -> second);
+				empty = false;
+				group_hps.erase(allocated);
+				req_hp -= (allocated -> first);
+				allocated = getCeil(group_hps, req_hp);
+			}
+			if (empty)
+			{
+				actor_splits[i].append(group_hps.begin() -> second);
+				group_hps.erase(group_hps.begin())
+			}
+		}
+		for (auto actors : actor_splits) Group(actors);
+		groups.erase(groups.find(*this -> group_id));
+	}
+
+	void merge(vector <Group*> grps, GroupState* new_state=GroupState())
+	{
+		list_act_id_t new_actors;
+		for (auto &group : grps)
+		{
+			for (auto &actor : group -> group_actors) 
+				new_actors.append(actor);
+		}
+		Group(new_actors, new_state);
+		for (auto &old_grp : grps) groups.erase(groups.find(old_grp -> group_id));
+	}
+
+	vector <int> clipExtra(vector <int> hps, int extra)
+	{
+		int residueOfEach = extra / hps.size();
+		for (auto &hp: hps) hp -= residueOfEach;   // assuming this remains +ve
+		return hps;
+	}
+
+	int getCeil(set < pair <int, act_id_t> > hps, x) 
+	{
+		set < pair <int, act_id_t> > :: iterator it = hps.lower_bound(x);
+		if ((*it) -> first > x)
+		{
+			if (it == hps.begin()) return 0;
+			return (--it) -> first;
+		}
+		return it -> first;
 	}
 
 	virtual void AttackUtility() {};
@@ -74,6 +146,7 @@ public:
 class GroupState
 {
 public:
+	GroupState() {}; // this constructor should set the default starting state
 	virtual void update() {};
 }	
 
@@ -83,7 +156,7 @@ public:
 	GroupState* SelectState() {
 		float MaxUtility = MaxScore(AttackUtility(),RetreatUtility(),ExploreUtility(),GuardUtility());
 		if(AttackUtility == MaxUtility)
-			return null;
+			return NULL;
 		else if(RetreatUtility == MaxUtility)
 			return new Retreat();
 		else if(ExploreUtility == MaxUtility)
@@ -108,7 +181,7 @@ public:
 		else if(RetreatUtility == MaxUtility)
 			return new Retreat();
 		else if(ExploreUtility == MaxUtility)
-			return null;
+			return NULL;
 		else if(GuardUtility == MaxUtility)
 			return new Guard();
 	}
@@ -127,9 +200,9 @@ public:
 		if(AttackUtility == MaxUtility)
 			return new Attack();
 		else if(RetreatUtility == MaxUtility)
-			return null;
+			return NULL;
 		else if(ExploreUtility == MaxUtility)
-			return null;
+			return NULL;
 		else if(GuardUtility == MaxUtility)
 			return new Guard();
 	}
@@ -152,7 +225,7 @@ public:
 		else if(ExploreUtility == MaxUtility)
 			return Explore();
 		else if(GuardUtility == MaxUtility)
-			return null;
+			return NULL;
 	}
 	void GuardStrategy() {};
 	void AttackUtility() {};
