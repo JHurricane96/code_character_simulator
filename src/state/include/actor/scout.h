b7cb18687415bef3d5d3eab9285970929f21699a
Soorya Narayan <nayaran.ayroos@gmail.com>
2017-01-13 08:06:13 +0530

    Add Scout class declaration

--- src/state/include/actor/scout.h
+++ src/state/include/actor/scout.h
@@ -8,7 +8,6 @@
 
 #include <memory>
 #include "actor/actor.h"
-#include "path_planner/path_planner_helper.h"
 #include "state_export.h"
 
 namespace state {
@@ -16,25 +15,34 @@ namespace state {
  * Class for scout
  *
  * The scout cannot attack and is invisible to the enemy unless he is
- * within the line of site of the base
+ * within the line of site of the base or a tower
  */
 class STATE_EXPORT Scout: public Actor {
 public:
-	Scout();
-	/**
-	 * An instance of the PathPlannerHelper class to help with path planning
-	 */
-	PathPlannerHelper path_planner_helper;
-	/**
-	 * Gets the path planner helper
-	 *
-	 * @return     The path planner helper
-	 */
-	const std::unique_ptr<PathPlannerHelper> GetPathPlannerHelper();
+	Scout(
+		act_id_t id,
+		PlayerId player_id,
+		int64_t attack,
+		int64_t hp,
+		int64_t max_hp,
+		int64_t max_speed,
+		int64_t size,
+		int64_t total_respawn_time,
+		float time_to_respawn,
+		int64_t time_spent_near_base,
+		physics::Vector2D position,
+		physics::Vector2D velocity,
+		int64_t los_radius,
+		int64_t attack_speed,
+		int64_t attack_range
+	);
 	/**
 	 * Update function to be called every tick
+	 *
+	 * @param[in]  delta_time  The difference in time between the
+	 *                         previous and current Update calls
 	 */
-	void Update();
+	void Update(float delta_time);
 };
 
 }
