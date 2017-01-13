f2965c5f8399d645c280882312c64a1c908e1a18
Soorya Narayan <nayaran.ayroos@gmail.com>
2017-01-13 08:07:23 +0530

    Add Scout class definitions

---
+++ src/state/src/actor/scout.cpp
@@ -0,0 +1,46 @@
+#include "actor/scout.h"
+
+namespace state {
+
+Scout::Scout(
+		act_id_t id,
+		PlayerId player_id,
+		int64_t attack,
+		int64_t hp,
+		int64_t max_hp,
+		int64_t max_speed,
+		int64_t size,
+		int64_t total_respawn_time,
+		float time_to_respawn,
+		int64_t time_spent_near_base,
+		physics::Vector2D position,
+		physics::Vector2D velocity,
+		int64_t los_radius,
+		int64_t attack_speed,
+		int64_t attack_range
+	):
+	Actor(
+		id,
+		player_id,
+		attack,
+		hp,
+		max_hp,
+		max_speed,
+		size,
+		total_respawn_time,
+		time_to_respawn,
+		time_spent_near_base,
+		position,
+		velocity,
+		los_radius,
+		attack_speed,
+		attack_range,
+		ActorType::SCOUT
+	) {}
+
+void Scout::Update(float delta_time) {
+	DecideState(delta_time);
+	position = position + velocity * delta_time;
+}
+
+}
