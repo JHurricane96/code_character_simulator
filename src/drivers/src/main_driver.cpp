2fed12b5ed7a6077224fcc0c12d87ae2d10054eb
JHurricane96 <arun.jhurricane@gmail.com>
2017-01-30 17:52:11 +0530

    Wrap utilities.h contents in state namespace
    Add namespace scoping of PlayerId enums to main_driver.cpp

--- src/drivers/src/main_driver.cpp
+++ src/drivers/src/main_driver.cpp
@@ -15,8 +15,8 @@ MainDriver::MainDriver(
 	game_state(s1),
 	p1_state_buffer(s2),
 	p2_state_buffer(s3),
-	p1_buffer(new state::PlayerStateHandler(p1_state_buffer.get(), PLAYER1)),
-	p2_buffer(new state::PlayerStateHandler(p2_state_buffer.get(), PLAYER2)),
+	p1_buffer(new state::PlayerStateHandler(p1_state_buffer.get(), state::PLAYER1)),
+	p2_buffer(new state::PlayerStateHandler(p2_state_buffer.get(), state::PLAYER2)),
 	p1_driver(new PlayerDriver(p1_buffer, p1_code)),
 	p2_driver(new PlayerDriver(p2_buffer, p2_code)),
 	game_over(false) {}
@@ -29,11 +29,11 @@ void MainDriver::GlobalUpdateLoop() {
 		}
 		modified1 = modified2 = false;
 		if(p1_driver->GetIsModifyDone()) {
-			game_state->MergeWithBuffer(*p1_state_buffer, PLAYER1);
+			game_state->MergeWithBuffer(*p1_state_buffer, state::PLAYER1);
 			modified1 = true;
 		}
 		if (p2_driver->GetIsModifyDone()) {
-			game_state->MergeWithBuffer(*p2_state_buffer, PLAYER2);
+			game_state->MergeWithBuffer(*p2_state_buffer, state::PLAYER2);
 			modified2 = true;
 		}
 		game_state->Update(0.8);
