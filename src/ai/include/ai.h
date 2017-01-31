96e6da296b014d2a397eda03b5487bb7b30820fb
JHurricane96 <arun.jhurricane@gmail.com>
2017-02-01 02:29:23 +0530

    Bring AI to buildable state

---
+++ src/ai/include/ai.h
@@ -0,0 +1,25 @@
+#ifndef AI_AI_H
+#define AI_AI_H
+
+#include <memory>
+#include <map>
+#include "player_ai_helper.h"
+#include "ai_export.h"
+
+namespace ai {
+
+class Group;
+
+class AI_EXPORT AI : public player::PlayerAiHelper {
+private:
+	bool init_groups;
+	state::list_act_id_t all_actors;
+	std::vector <Group*> groups;
+public:
+	AI();
+	void Update(std::shared_ptr<state::PlayerStateHandler> state) override;
+};
+
+}
+
+#endif
---
+++ src/ai/include/attack_rules.h
@@ -0,0 +1,41 @@
+/**
+ * @file attack_rules.h
+ * Implementation file of the Attack Strategies and Utilities for a Single Actor Unit per tick
+ */
+#ifndef ATTACK_RULES_H
+#define ATTACK_RULES_H
+
+#include <memory>
+#include "player_state_handler/player_state_handler.h"
+
+namespace ai {
+
+/**
+ * Class for attack rules for guard strategies and utilities
+ */
+class AttackRules {
+	/**
+	 * The formation generally used for attack
+	 */
+	state::FormationMaker *formation;
+public:
+	AttackRules();
+	/**
+	 * The strategy for the attack per unit per tick
+	 *
+	 * @param[in]  unitId  The unit identifier
+	 */
+	void Strategy(state::act_id_t unitId, std::shared_ptr<state::PlayerStateHandler> state);
+	/**
+	 * The utility function or the transition specifier for the Attack state
+	 *
+	 * @param[in]  unitId  The unit identifier
+	 *
+	 * @return     int var (1-GoToAtack, 2-GoToRetreat, 3-GoToExplore, 4-GoToGuard)
+	 */
+	int Utility(state::act_id_t unitId, std::shared_ptr<state::PlayerStateHandler> state);
+};
+
+}
+
+#endif
