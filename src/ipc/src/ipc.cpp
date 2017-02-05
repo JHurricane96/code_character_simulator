8255985c1a02d1ba4977fcceb1d16cd6c262b971
Sachin <sachinashok.g@gmail.com>
2017-02-05 23:23:15 +0530

    Make logger thread safe

--- src/ipc/src/ipc.cpp
+++ src/ipc/src/ipc.cpp
@@ -1,63 +1,65 @@
 #include <ipc.h>
+#include <mutex>
 
 namespace ipc {
 
-	Interrupts::Interrupts() :
-		play(false),
-		level(1),
-		exit(false),
-		restart(false) {}
+Interrupts::Interrupts() :
+	play(false),
+	level(1),
+	exit(false),
+	restart(false) {}
 
-	bool Interrupts::GetPlayStatus() {
-		return play.load();
-	}
+bool Interrupts::GetPlayStatus() {
+	return play.load();
+}
 
-	int Interrupts::GetLevelNumber() {
-		return level.load();
-	}
+int Interrupts::GetLevelNumber() {
+	return level.load();
+}
 
-	bool Interrupts::GetExitStatus() {
-		return exit.load();
-	}
+bool Interrupts::GetExitStatus() {
+	return exit.load();
+}
 
-	bool Interrupts::GetRestartStatus() {
-		return restart.load();
-	}
+bool Interrupts::GetRestartStatus() {
+	return restart.load();
+}
 
-	void Interrupts::SetPlayStatus(bool play_status) {
-		play = play_status;
-	}
+void Interrupts::SetPlayStatus(bool play_status) {
+	play = play_status;
+}
 
-	void Interrupts::SetLevelNumber(int current_level) {
-		level = current_level;
-	}
+void Interrupts::SetLevelNumber(int current_level) {
+	level = current_level;
+}
 
-	void Interrupts::SetExitStatus(bool exit_status) {
-		exit = exit_status;
-	}
+void Interrupts::SetExitStatus(bool exit_status) {
+	exit = exit_status;
+}
 
-	void Interrupts::SetRestartStatus(bool restart_status) {
-		restart = restart_status;
-	}
+void Interrupts::SetRestartStatus(bool restart_status) {
+	restart = restart_status;
+}
 
-	std::vector<std::string> Logger::GetLogs() {
-			return logs;
-	}
+std::vector<std::string> Logger::GetLogs() {
+	std::lock_guard<std::mutex> lock(logmutex);
+	return logs;
+}
 
-	void Logger::SetLogs(std::string log) {
-		logs.push_back(log);
-	}
+void Logger::SetLogs(std::string log) {
+	std::lock_guard<std::mutex> lock(logmutex);
+	logs.push_back(log);
+}
 
-	void Logger::EmptyLogs() {
-		logs.clear();
-	}
+void Logger::EmptyLogs() {
+	std::lock_guard<std::mutex> lock(logmutex);
+	logs.clear();
+}
 
-	Logger* Logger::Instance() {
-		if(!static_instance)
-			static_instance = new Logger;
-		return static_instance;
-	}
+Logger& Logger::Instance() {
 
-	Logger *Logger::static_instance = nullptr;
+	static Logger static_instance;
+	return static_instance;
+}
 
 }
