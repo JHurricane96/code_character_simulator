2055a7e0b297a45f5d627f918a1daa2523840d66
cheeni666 <vshreenivasbharadwaj@gmail.com>
2017-02-07 22:02:36 +0530

    TEST

--- src/ai/include/guard_rules.h
+++ src/ai/include/guard_rules.h
@@ -1,11 +1,18 @@
 /**
  * @file guard_rules.h
- * Implementation file for defence strategies
+ * Headers file for defence strategies
  */
+
 #ifndef GUARD_RULES_H
 #define GUARD_RULES_H
 
 #include <memory>
+#include <vector>
+#include <bits/stdc++.h>
+
+#include "state.h"
+#include "ai_utilities.h"
+#include "utilities.h"
 #include "player_state_handler/player_state_handler.h"
 
 namespace ai {
@@ -18,26 +25,26 @@ class GuardRules {
 	 * The formation generally used for guarding
 	 */
 	state::FormationMaker *formation;
-
+	std::unique_ptr<std::vector<state::act_id_t>> kingsGuard;
+	std::unique_ptr<std::vector<state::act_id_t>> flagsGuard;
 public:
-
-	GuardRules();
-
+	/**
+	 * Constructor for the GuardRules class
+	 *
+	 * @param      formation       The formation
+	 * @param[in]  kingsGuardParam  The kings guard param
+	 * @param[in]  flagsGuardParam  The flags guard param
+	 */
+	GuardRules(std::unique_ptr<std::vector<state::act_id_t>> kingsGuardParam, std::unique_ptr<std::vector<state::act_id_t>> flagsGuardParam, state::FormationMaker *formationParam);
 	/**
 	 * The strategy for guarding per unit per tick
 	 *
-	 * @param[in]  unitId  The unit identifier
+	 * @param[in]  unitId            The unit identifier
+	 * @param[in]  state             The state
+	 * @param      groupStateHolder  The group state holder
 	 */
-	void Strategy(state::act_id_t unitId, std::shared_ptr<state::PlayerStateHandler> state);
-
-	/**
-	 * The utility function or the transition specifier for the guard state
-	 *
-	 * @param[in]  unitId  The unit identifier
-	 *
-	 * @return     int var (1-GoToAtack, 2-GoToRetreat, 3-GoToExplore, 4-GoToGuard)
-	 */
-	int Utility(state::act_id_t unitId, std::shared_ptr<state::PlayerStateHandler> state);
+	void Strategy(state::act_id_t unitId, std::shared_ptr<state::PlayerStateHandler> state, int &groupStateHolder);
+	int Utility(int &groupStateHolder);
 };
 
 }
