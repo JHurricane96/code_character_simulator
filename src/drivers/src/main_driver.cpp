7f9ad558864b2c3a0f4961005861e91d8cc9d842
JHurricane96 <arun.jhurricane@gmail.com>
2017-02-07 17:40:22 +0530

    Add headless mode to simulator
    A command-line argument is passed to the main executable
    to specify the mode
    
    * Add MainDriver::is_headless
      This flag is initialized by a constructor using an argument
    
    * Add MainDriver::GlobalUpdateLoopHeadless
      This loop runs the driver in headless mode, and doesn't do IPC
    
    * Modify MainDriver::Run to check for whether to run in headless mode
    
    * Check for command line argument in main executable
      It initializes MainDriver based on the cmd line arg

--- src/drivers/src/main_driver.cpp
+++ src/drivers/src/main_driver.cpp
@@ -13,7 +13,8 @@ MainDriver::MainDriver(
 	std::shared_ptr<state::State> s1,
 	std::shared_ptr<state::State> s2,
 	std::shared_ptr<state::State> s3,
-	int64_t total_game_duration) :
+	int64_t total_game_duration,
+	bool is_headless) :
 	game_state(s1),
 	p1_state_buffer(s2),
 	p2_state_buffer(s3),
@@ -23,7 +24,8 @@ MainDriver::MainDriver(
 	p2_driver(new PlayerDriver(p2_buffer, p2_code)),
 	game_over(false),
 	total_game_duration(total_game_duration),
-	fps(30) {}
+	fps(30),
+	is_headless(is_headless) {}
 
 void MainDriver::GlobalUpdateLoop() {
 	bool modified1, modified2;
@@ -106,6 +108,64 @@ void MainDriver::GlobalUpdateLoop() {
 	Stop();
 }
 
+void MainDriver::GlobalUpdateLoopHeadless() {
+	bool modified1, modified2;
+
+	std::chrono::milliseconds game_duration(0);
+
+	auto prev_time = std::chrono::high_resolution_clock::now();
+
+	while(1) {
+		if (game_over) {
+			break;
+		}
+
+		auto start_time = std::chrono::high_resolution_clock::now();
+		auto update_duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+			start_time - prev_time
+		);
+		game_duration += update_duration;
+		prev_time = start_time;
+
+		modified1 = modified2 = false;
+		if(p1_driver->GetIsModifyDone()) {
+			game_state->MergeWithBuffer(*p1_state_buffer, state::PLAYER1);
+			modified1 = true;
+		}
+		if (p2_driver->GetIsModifyDone()) {
+			game_state->MergeWithBuffer(*p2_state_buffer, state::PLAYER2);
+			modified2 = true;
+		}
+		game_state->Update((float) update_duration.count() / fps);
+		if (modified1) {
+			p1_state_buffer->MergeWithMain(*game_state);
+		}
+		if (modified2) {
+			p2_state_buffer->MergeWithMain(*game_state);
+		}
+		if (modified1) {
+			p1_driver->SetIsModifyDone(false);
+		}
+		if (modified2) {
+			p2_driver->SetIsModifyDone(false);
+		}
+
+		if (game_duration.count() >= total_game_duration) {
+			break;
+		}
+
+		auto end_time = std::chrono::high_resolution_clock::now();
+		auto calculcation_duration =
+			std::chrono::duration_cast<std::chrono::milliseconds>(
+				end_time - start_time
+			);
+		std::this_thread::sleep_for(std::chrono::milliseconds(
+			std::max((long)0, 1000 / fps - calculcation_duration.count()))
+		);
+	}
+	Stop();
+}
+
 void MainDriver::Run() {
 	game_state->Update(1);
 	p1_state_buffer->MergeWithMain(*game_state);
@@ -113,7 +173,12 @@ void MainDriver::Run() {
 
 	p1_driver->Run();
 	p2_driver->Run();
-	runner = std::thread(&MainDriver::GlobalUpdateLoop, this);
+	if (!is_headless) {
+		runner = std::thread(&MainDriver::GlobalUpdateLoop, this);
+	}
+	else {
+		runner = std::thread(&MainDriver::GlobalUpdateLoopHeadless, this);
+	}
 }
 
 void MainDriver::Join() {
