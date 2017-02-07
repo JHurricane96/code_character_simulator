2700d306f38daec96da001f93dc8f9b00706f047
cheeni666 <vshreenivasbharadwaj@gmail.com>
2017-02-08 04:06:23 +0530

    Changed Optimal Target in Guard

--- src/ai/include/ai_utilities.h
+++ src/ai/include/ai_utilities.h
@@ -146,8 +146,8 @@ bool IsEnemyTowerDominating(std::shared_ptr<state::PlayerStateHandler> state);
 state::act_id_t GetOptimalTarget (
 	std::shared_ptr<state::PlayerStateHandler> state,
 	state::act_id_t unitId,
-	float threshold,
-	std::vector<std::pair<int64_t, int>> sortedEnemies
+	std::vector<std::pair<int64_t, int>> sortedEnemies,
+	float threshold = -1
 );
 
 }
